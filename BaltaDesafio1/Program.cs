080b29f4f84268b81d0746c663e0a7d81c2d4744
felipe.pestana <felipe.pestana@SITNOTT01>
2023-02-20 11:45:13 -0300

    Add project files.

---
+++ BaltaDesafio1/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Digite um texto: ");
+        string txt = Console.ReadLine();
+
+        Console.WriteLine("Esse foi o texto informado: "+txt);
+        Console.WriteLine("O texto possui " + txt.Length + " caracteres");
+        Console.WriteLine("E " + txt.Split(' ').Length + " palavras...");
+    }
+}
